af025308cdb4b577c694aa3ffa304c5fc51e2a50
sergey1 <seregin7272@mail.ru>
2018-04-14 16:50:28 +0500

    22

---
+++ app/Mail/Auth/VerifyMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail\Auth;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Entity\User;
+
+class VerifyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('VerifyMail')
+            ->markdown('emails.users.verify');
+    }
+}
