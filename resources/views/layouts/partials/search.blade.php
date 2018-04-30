d2065776efbfe2b154e97cfbebdfea7b93dad7de
sergey1 <seregin7272@mail.ru>
2018-04-30 17:18:26 +0500

    added attributes form search

--- resources/views/layouts/partials/search.blade.php
+++ resources/views/layouts/partials/search.blade.php
@@ -11,10 +11,45 @@
                         </div>
                         <div class="col-md-1">
                             <div class="form-group">
-                                <button class="btn btn-light border" type="submit"><span class="fa fa-search"></span></button>
+                                <button class="btn btn-light border" type="submit">Найти</button>
                             </div>
                         </div>
                     </div>
+
+                    @if ($category)
+                        <div class="row">
+                            @foreach ($category->allAttributes() as $attribute)
+                                @if ($attribute->isSelect() || $attribute->isNumber())
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label class="col-form-label">{{ $attribute->name }}</label>
+
+                                            @if ($attribute->isSelect())
+                                                <select class="form-control" name="attrs[{{ $attribute->id }}][equals]">
+                                                    <option value=""></option>
+                                                    @foreach ($attribute->variants as $variant)
+                                                        <option value="{{ $variant }}"{{ $variant === request()->input('attrs.' . $attribute->id . '.equals') ? ' selected' : '' }}>
+                                                            {{ $variant }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+
+                                            @elseif ($attribute->isNumber())
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <input type="number" class="form-control" name="attrs[{{ $attribute->id }}][from]" value="{{ request()->input('attrs.' . $attribute->id . '.from') }}" placeholder="From">
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <input type="number" class="form-control" name="attrs[{{ $attribute->id }}][to]" value="{{ request()->input('attrs.' . $attribute->id . '.to') }}" placeholder="To">
+                                                    </div>
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    @endif
                 </form>
             </div>
             <div class="col-md-3" style="text-align: right">
